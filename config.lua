ac1306e5137ced2039588633aa68508c68627b4f
MaDHouSe <madhouse1979@gmail.com>
2024-04-19 22:06:04 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,6 +6,11 @@ Config = {}
 Config.Notify = "qb-core" -- Default:(qb-core), you can also use (roda-notify/okokNotify)
 Config.NotifyTitle = "Stash System"
 
+-- this ignores not used props for this script
+Config.IgnorePropsInHand = {
+    "v_ind_cf_chckbox2",
+}
+
 Config.Shop = {
     model = "g_m_y_korean_01",
     senario = "WORLD_HUMAN_STAND_MOBILE",
